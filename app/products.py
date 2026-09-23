4dfb682559d5a07027d8f957b7035bb92b77588f
shkuhub <k.seheeku@gmail.com>
2026-09-23 10:44:54 +0900

    Add app/products.py

---
+++ app/products.py
@@ -0,0 +1,18 @@
+PRODUCTS = [
+    {
+        "name": "민음사)모카번(커스타드)",
+        "item_code": None,
+    },
+    {
+        "name": "민음사)모카번(우유크림)",
+        "item_code": None,
+    },
+    {
+        "name": "민음사)깨찰빵(커스타드)",
+        "item_code": None,
+    },
+    {
+        "name": "민음사)깨찰빵(솔티밀크)",
+        "item_code": None,
+    },
+]
